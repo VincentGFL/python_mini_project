d99e25337392b49f3c12a636177572d0c533ea07
Vincent <linguofu303@gmail.com>
2020-11-26 14:34:49 +0000

    Add python -m to command

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 pip3 install pytest
-python -m pytest --version
-python -m pytest ./service1 --cov ./service1/application
-python -m pytest ./service2 --cov ./service2/application
+python3 -m pytest ./service1 --cov ./service1/application
+python3 -m pytest ./service2 --cov ./service2/application
