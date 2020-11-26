f1b65c7156190cb1ad976f660d0bf8934dd1429c
Vincent <linguofu303@gmail.com>
2020-11-26 16:06:17 +0000

    Rollback

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 pip3 install pytest
 python3 -m pytest ./service1 --cov ./service1/application
-pytest ./service2 --cov ./service2/application
+python3 -m pytest ./service2 --cov ./service2/application
